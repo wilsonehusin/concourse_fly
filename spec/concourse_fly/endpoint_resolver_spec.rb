eadd6003b92bbf44f35832f9a95b2abeec8ea39d
Wilson E. Husin <wilsonehusin@gmail.com>
2020-04-22 15:21:11 -0700

    Spec refactoring with WebMock

--- spec/concourse_fly/endpoint_resolver_spec.rb
+++ spec/concourse_fly/endpoint_resolver_spec.rb
@@ -2,6 +2,10 @@ module ConcourseFly
   RSpec.describe EndpointResolver do
     subject { EndpointResolver.new "v5.8.0" }
     context "#fetch" do
+      before :each do
+        stub_request(:get, /github/)
+          .to_return(body: '{Path: "/api/v1/info", Method: "GET", Name: "GetInfo"},')
+      end
       context "when cached file is available" do
         before :each do
           allow(File).to receive(:exist?).and_return(true)
@@ -10,30 +14,26 @@ module ConcourseFly
             foo: bar
           YAML
         end
-        it "retrieves from cached file and doesn't make HTTP request" do
+        it "returns the value from cached file" do
           expect(subject.fetch).to eq({"foo" => "bar"})
-        end
-        it "does not make HTTP get" do
-          expect(Faraday).not_to receive(:get)
-          subject.fetch
         end
       end
       context "when cached file is unavailable" do
-        it "retrieves from Concourse repository" do
+        before :each do
           allow(File).to receive(:exist?).and_return(false)
-          expect(Faraday).to receive(:get).and_return(
-            double("response", body: '{Path: "/api/v1/info", Method: "GET", Name: "GetInfo"},')
-          )
+        end
+        it "retrieves from Concourse repository" do
           expect(subject.fetch).to eq({"GetInfo" => {"path" => "/api/v1/info", "method" => "GET"}})
         end
       end
       context "when ignore_cache is true" do
-        it "retrieves from Concourse repository" do
+        before :each do
           allow(File).to receive(:exist?).and_return(true)
-          expect(Faraday).to receive(:get).and_return(
-            double("response", body: '{Path: "/api/v1/info", Method: "GET", Name: "GetInfo"},')
-          )
+        end
+        it "does not read from cached file" do
           expect(File).not_to receive(:read)
+        end
+        it "retrieves from Concourse repository" do
           expect(subject.fetch(false)).to eq({"GetInfo" => {"path" => "/api/v1/info", "method" => "GET"}})
         end
       end
